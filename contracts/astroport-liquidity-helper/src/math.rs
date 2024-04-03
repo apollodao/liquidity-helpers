45b221c41059655386b49963b47a4cdddb26c6c9
Pacman <pacman@apollo.farm>
2024-04-03 09:54:03 -0700

    wip: implement BigDecimal

--- contracts/astroport-liquidity-helper/src/math.rs
+++ contracts/astroport-liquidity-helper/src/math.rs
@@ -2,41 +2,406 @@
 
 use apollo_cw_asset::Asset;
 use astroport_v3::pair_xyk_sale_tax::TaxConfigsChecked;
-use cosmwasm_std::{Decimal, Decimal256, Int256, StdError, StdResult, Uint128, Uint256};
+use cosmwasm_std::{Decimal, Deps, Int256, StdError, StdResult, Uint128, Uint256};
+use cw_bigint::BigInt;
 
-fn mul(a: Int256, b: Decimal256) -> Int256 {
-    match a.cmp(&Int256::zero()) {
-        std::cmp::Ordering::Equal => Int256::zero(),
-        std::cmp::Ordering::Greater => {
-            let a = Uint256::try_from(a).unwrap();
-            let b = a * b;
+use crate::math::big_decimal::{bigint_to_u128, BigDecimal};
 
-            Int256::try_from(b).unwrap()
+pub mod big_decimal {
+    use std::ops::{Add, Deref, Div, Mul, Sub};
+
+    use cosmwasm_std::{Decimal, Fraction, StdError, StdResult, Uint128};
+    use cw_bigint::BigInt;
+
+    pub const BIG_DECIMAL_FRACTIONAL: u128 = 1_000_000_000_000_000_000;
+
+    #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct BigDecimal(BigInt);
+
+    impl BigDecimal {
+        pub fn zero() -> Self {
+            Self(BigInt::from(0u128))
         }
-        std::cmp::Ordering::Less => {
-            let a = Uint256::try_from(-a).unwrap();
-            let b = a * b;
 
-            -Int256::try_from(b).unwrap()
+        pub fn one() -> Self {
+            Self(BigInt::from(BIG_DECIMAL_FRACTIONAL))
+        }
+
+        /// Returns the square root of the BigDecimal.
+        ///
+        /// Uses the Newton-Raphson method to approximate the square root.
+        ///
+        /// # Panics
+        /// If the BigDecimal is negative, this function will panic.
+        pub fn sqrt(&self) -> Self {
+            if self < &Self::zero() {
+                panic!("Cannot compute the square root of a negative number.");
+            }
+            if self == &Self::zero() {
+                return Self::zero();
+            }
+
+            let mut x = self.clone();
+            let mut y = (x.clone() + Self::one()) / BigDecimal::from(2u128);
+            while y < x {
+                x = y.clone();
+                y = (x.clone() + self.clone() / x.clone()) / BigDecimal::from(2u128);
+            }
+            y
+        }
+
+        pub fn pow(&self, exp: u32) -> Self {
+            if exp == 0 {
+                return Self::one();
+            }
+            if exp == 1 {
+                return self.clone();
+            }
+
+            // BigDecimal is a fixed-point number with BIG_DECIMAL_FRACTIONAL decimal places.
+            // x^y = (numerator / denominator)^y = numerator^y / denominator^y
+            //     = (numerator^y / denominator^(y-1)) / denominator
+            // which means we represent the new number as new_numerator = numerator^y / denominator^(y-1),
+            // and new_denominator = denominator.
+            let value: BigInt = self.0.pow(exp) / BIG_DECIMAL_FRACTIONAL.pow(exp - 1);
+
+            Self(value)
+        }
+
+        pub fn floor(&self) -> BigInt {
+            self.0.clone() / BIG_DECIMAL_FRACTIONAL
         }
     }
-}
 
-/// Returns square root of a BigInt
-fn int256_sqrt(input: Int256) -> StdResult<Int256> {
-    if input < 0.into() {
-        return Err(StdError::generic_err(
-            "Cannot calculate square root of negative number",
-        ));
+    impl Mul for BigDecimal {
+        type Output = Self;
+
+        fn mul(self, rhs: Self) -> Self::Output {
+            Self(self.0 * rhs.0 / BIG_DECIMAL_FRACTIONAL)
+        }
     }
 
-    let mut x = input.clone();
-    let mut y = (x + Int256::one()) / Int256::from(2u128);
-    while y < x {
-        x = y;
-        y = (&x + &input / &x) / Int256::from(2u128);
+    impl<'a, 'b> Mul<&'b BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: &'b BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() * rhs.0.clone() / BIG_DECIMAL_FRACTIONAL)
+        }
     }
-    Ok(x)
+
+    impl<'a> Mul<BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() * rhs.0 / BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl<'a> Mul<&'a BigDecimal> for BigDecimal {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: &'a BigDecimal) -> Self::Output {
+            BigDecimal(self.0 * rhs.0.clone() / BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl Mul<BigInt> for BigDecimal {
+        type Output = Self;
+
+        fn mul(self, rhs: BigInt) -> Self::Output {
+            Self(self.0 * rhs)
+        }
+    }
+
+    impl<'a> Mul<&'a BigInt> for BigDecimal {
+        type Output = Self;
+
+        fn mul(self, rhs: &'a BigInt) -> Self::Output {
+            Self(self.0 * rhs.clone())
+        }
+    }
+
+    impl<'a> Mul<BigInt> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: BigInt) -> Self::Output {
+            BigDecimal(self.0.clone() * rhs)
+        }
+    }
+
+    impl<'a, 'b> Mul<&'b BigInt> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: &'b BigInt) -> Self::Output {
+            BigDecimal(self.0.clone() * rhs.clone())
+        }
+    }
+
+    impl Mul<BigDecimal> for BigInt {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: BigDecimal) -> Self::Output {
+            BigDecimal(self * rhs.0)
+        }
+    }
+
+    impl<'a> Mul<&'a BigDecimal> for BigInt {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: &'a BigDecimal) -> Self::Output {
+            BigDecimal(self * rhs.0.clone())
+        }
+    }
+
+    impl<'a> Mul<BigDecimal> for &'a BigInt {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: BigDecimal) -> Self::Output {
+            BigDecimal(self.clone() * rhs.0)
+        }
+    }
+
+    impl<'a, 'b> Mul<&'a BigDecimal> for &'b BigInt {
+        type Output = BigDecimal;
+
+        fn mul(self, rhs: &'a BigDecimal) -> Self::Output {
+            BigDecimal(self.clone() * rhs.0.clone())
+        }
+    }
+
+    impl Add for BigDecimal {
+        type Output = Self;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            Self(self.0 + rhs.0)
+        }
+    }
+
+    impl<'a, 'b> Add<&'b BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: &'b BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() + rhs.0.clone())
+        }
+    }
+
+    impl<'a> Add<BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() + rhs.0)
+        }
+    }
+
+    impl<'a> Add<&'a BigDecimal> for BigDecimal {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: &'a BigDecimal) -> Self::Output {
+            BigDecimal(self.0 + rhs.0.clone())
+        }
+    }
+
+    impl Add<BigInt> for BigDecimal {
+        type Output = Self;
+
+        fn add(self, rhs: BigInt) -> Self::Output {
+            Self(self.0 + rhs * BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl<'a, 'b> Add<&'b BigInt> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: &'b BigInt) -> Self::Output {
+            BigDecimal(self.0.clone() + rhs.clone() * BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl Add<BigDecimal> for BigInt {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: BigDecimal) -> Self::Output {
+            rhs + self
+        }
+    }
+
+    impl<'a> Add<&'a BigDecimal> for BigInt {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: &'a BigDecimal) -> Self::Output {
+            rhs + self
+        }
+    }
+
+    impl<'a> Add<BigInt> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: BigInt) -> Self::Output {
+            self.clone() + rhs
+        }
+    }
+
+    impl<'a, 'b> Add<&'a BigDecimal> for &'b BigInt {
+        type Output = BigDecimal;
+
+        fn add(self, rhs: &'a BigDecimal) -> Self::Output {
+            rhs + self.clone()
+        }
+    }
+
+    impl Sub for BigDecimal {
+        type Output = Self;
+
+        fn sub(self, rhs: Self) -> Self::Output {
+            Self(self.0 - rhs.0)
+        }
+    }
+
+    impl<'a, 'b> Sub<&'b BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn sub(self, rhs: &'b BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() - rhs.0.clone())
+        }
+    }
+
+    impl<'a> Sub<BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn sub(self, rhs: BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() - rhs.0)
+        }
+    }
+
+    impl<'a> Sub<&'a BigDecimal> for BigDecimal {
+        type Output = BigDecimal;
+
+        fn sub(self, rhs: &'a BigDecimal) -> Self::Output {
+            BigDecimal(self.0 - rhs.0.clone())
+        }
+    }
+
+    impl Sub<BigInt> for BigDecimal {
+        type Output = Self;
+
+        fn sub(self, rhs: BigInt) -> Self::Output {
+            Self(self.0 - rhs * BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl<'a, 'b> Sub<&'a BigInt> for &'b BigDecimal {
+        type Output = BigDecimal;
+
+        fn sub(self, rhs: &'a BigInt) -> Self::Output {
+            BigDecimal(self.0.clone() - rhs.clone() * BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl Sub<BigDecimal> for BigInt {
+        type Output = BigDecimal;
+
+        fn sub(self, rhs: BigDecimal) -> Self::Output {
+            BigDecimal(self * BIG_DECIMAL_FRACTIONAL - rhs.0)
+        }
+    }
+
+    impl<'a, 'b> Sub<&'a BigDecimal> for &'b BigInt {
+        type Output = BigDecimal;
+
+        fn sub(self, rhs: &'a BigDecimal) -> Self::Output {
+            BigDecimal(self.clone() * BIG_DECIMAL_FRACTIONAL - rhs.0.clone())
+        }
+    }
+
+    impl Div for BigDecimal {
+        type Output = Self;
+
+        fn div(self, rhs: Self) -> Self::Output {
+            Self(self.0 * BIG_DECIMAL_FRACTIONAL / rhs.0)
+        }
+    }
+
+    impl<'a, 'b> Div<&'b BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn div(self, rhs: &'b BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() * BIG_DECIMAL_FRACTIONAL / rhs.0.clone())
+        }
+    }
+
+    impl<'a> Div<BigDecimal> for &'a BigDecimal {
+        type Output = BigDecimal;
+
+        fn div(self, rhs: BigDecimal) -> Self::Output {
+            BigDecimal(self.0.clone() * BIG_DECIMAL_FRACTIONAL / rhs.0)
+        }
+    }
+
+    impl<'a> Div<&'a BigDecimal> for BigDecimal {
+        type Output = BigDecimal;
+
+        fn div(self, rhs: &'a BigDecimal) -> Self::Output {
+            BigDecimal(self.0 * BIG_DECIMAL_FRACTIONAL / rhs.0.clone())
+        }
+    }
+
+    impl From<BigInt> for BigDecimal {
+        fn from(value: BigInt) -> Self {
+            Self(value * BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl From<u128> for BigDecimal {
+        fn from(value: u128) -> Self {
+            Self(BigInt::from(value) * BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl From<Decimal> for BigDecimal {
+        fn from(value: Decimal) -> Self {
+            let value =
+                BigInt::from(value.numerator().u128()) * BigInt::from(value.denominator().u128());
+            Self(value / BIG_DECIMAL_FRACTIONAL)
+        }
+    }
+
+    impl TryFrom<BigDecimal> for Decimal {
+        type Error = StdError;
+
+        fn try_from(value: BigDecimal) -> Result<Self, Self::Error> {
+            let numerator: Uint128 = bigint_to_u128(&value.0)?.into();
+            Ok(Decimal::from_ratio(numerator, BIG_DECIMAL_FRACTIONAL))
+        }
+    }
+
+    pub fn bigint_to_u128(big_int: &BigInt) -> StdResult<u128> {
+        let (sign, bytes) = big_int.to_bytes_be();
+
+        if sign == cw_bigint::Sign::Minus {
+            return Err(StdError::generic_err(
+                "Cannot convert a negative BigInt number to u128",
+            ));
+        }
+
+        if bytes.len() > 16 {
+            return Err(StdError::generic_err(
+                "Attempting to convert BigInt to u128 with overflow",
+            ));
+        }
+
+        // Pad with zeros if length is less than 16
+        let num_of_zero_bytes = 16 - bytes.len();
+        let mut padded_bytes = vec![0u8; num_of_zero_bytes];
+        padded_bytes.extend_from_slice(&bytes);
+
+        Ok(u128::from_be_bytes(
+            padded_bytes
+                .as_slice()
+                .try_into()
+                .map_err(|e| StdError::generic_err(format!("Failed to convert to slice: {e}")))?,
+        ))
+    }
+
 }
 
 /// Calculate how much will be returned from a swap in a constant product pool
